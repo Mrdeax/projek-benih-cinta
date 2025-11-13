9e2ff12f6d94ebaa399a43141d0c8a42da0f1437
dravzxy <rajendravz.rvz@gmail.com>
2025-11-13 09:15:32 +0700

    hapus regist

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,9 +49,6 @@
             <button type="submit" class="btn btn-login">Login</button>
         </form>
 
-        <div class="register-link">
-            Belum punya akun? <a href="{{ route('register') }}">Daftar di sini</a>
-        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
